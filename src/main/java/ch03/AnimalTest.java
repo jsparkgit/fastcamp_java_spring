2c376a284cbe1a169477534d3426888f1f8a4905
jsparkgit <jspark.git@gmail.com>
2021-08-15 20:23:09 +0900

    20210822_ch03_08

--- src/main/java/ch03/AnimalTest.java
+++ src/main/java/ch03/AnimalTest.java
@@ -54,26 +54,55 @@ public class AnimalTest {
         Animal tAnimal = new Tiger();
         Animal eAnimal = new Eagle();
 
+/*
         AnimalTest test = new AnimalTest();
         test.moveAnimal(hAnimal);
         test.moveAnimal(tAnimal);
         test.moveAnimal(eAnimal);
+*/
 
-        System.out.println("================");
+//        System.out.println("================");
 
         ArrayList<Animal> animalList = new ArrayList<Animal>();
         animalList.add(hAnimal);
         animalList.add(tAnimal);
         animalList.add(eAnimal);
 
+/*
         for (Animal animal : animalList) {
             animal.move();
         }
+*/
+        AnimalTest test = new AnimalTest();
+        test.testDownCasting(animalList);
 
     }
 
     public void moveAnimal(Animal animal) {
         animal.move();
     }
+
+    public void testDownCasting(ArrayList<Animal> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            Animal animal = list.get(i);
+
+            if (animal instanceof Human) {
+                Human human = (Human) animal;
+                human.readBooks();
+            } else if (animal instanceof Tiger) {
+                Tiger tiger = (Tiger) animal;
+                tiger.hunting();
+            } else if (animal instanceof Eagle) {
+                Eagle eagle = (Eagle) animal;
+                eagle.flying();
+            } else {
+                System.out.println("Error");
+            }
+
+        }
+
+    }
+
 
 }
